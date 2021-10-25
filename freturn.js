6af5e8da4ac9d0ee36822be33dca02e64570e43c
Ivy Kathrina <88631468+newbiedev08@users.noreply.github.com>
2021-10-25 20:17:48 +0200

    Add files via upload

---
+++ freturn.js
@@ -0,0 +1,27 @@
+const bigNumber =function( number){
+    if (number > 100){
+    return 'true'}
+    else {
+         return 'false'
+    }
+};
+bigNumber(80);
+bigNumber(120);
+
+const bouncerBot = function(minAge, maxCap)
+const gastAge = 18;
+const currentNum = 30; {
+if (Age < 18){
+    return 'This is a club for adults'}
+if (Age >18 && currentNum < 50){
+    return 'Come in'}
+if (Age > 18 && currentNum > 50){
+   return  'Its too busy now, come back later';
+}
+};
+console.log();
+
+const number = [3,4,5,6,7];
+function getAverage (number){ 
+return number.reduce((3,4,5,6,7) = (3 + 4 + 5 + 6 + 7)) /number.length;
+}
